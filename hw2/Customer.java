32b49f7ccd640f3ebb3001ef6ea66dfbbfb6b884
Robert Schaedler III <robertschaedler3@gmail.com>
2020-10-03 16:44:46 -0400

    implement Customer.java

--- hw2/Customer.java
+++ hw2/Customer.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 
 public class Customer implements Runnable {
     private Bakery bakery;
@@ -13,21 +15,62 @@ public class Customer implements Runnable {
      * Initialize a customer object and randomize its shopping cart
      */
     public Customer(Bakery bakery) {
-        // TODO
+        this.bakery = bakery;
+        this.rnd = new Random();
+        this.shoppingCart = new ArrayList<>();
+
+        // Shop for 0-200 millis
+        this.shopTime = rnd.nextInt(200);
+
+        // Checkout for 0-200 millis
+        this.checkoutTime = rnd.nextInt(200);
     }
 
     /**
      * Run tasks for the customer
      */
     public void run() {
-        // TODO
+        try {
+            // Make a shopping list
+            this.fillShoppingCart();
+
+            // Customer is at the shelves
+            this.bakery.accessToShelves.acquire();
+
+            for (BreadType breadType : shoppingCart) {
+                Semaphore accessToShelf = this.bakery.accessToShelf.get(breadType);
+
+                accessToShelf.acquire();
+                Thread.sleep(shopTime / shoppingCart.size()); // Customer is shopping at a shelf
+                this.bakery.takeBread(breadType);
+                accessToShelf.release();
+            }
+
+            // Customer leaves shelves and moves to registers
+            this.bakery.accessToRegisters.acquire();
+            this.bakery.accessToShelves.release();
+
+            this.bakery.cashierMutex.acquire();
+            Thread.sleep(checkoutTime); // Customer is checking out
+            this.bakery.addSales(this.getItemsValue());
+            this.bakery.cashierMutex.release();
+
+            // Customer leaves the bakery
+            this.bakery.accessToRegisters.release();
+
+            System.out.println(this);
+
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+
     }
 
     /**
      * Return a string representation of the customer
      */
     public String toString() {
-        return "Customer " + hashCode() + ": shoppingCart=" + Arrays.toString(shoppingCart.toArray()) + ", shopTime=" + shopTime + ", checkoutTime=" + checkoutTime;
+        return String.format("Customer %10s : Cart=%-33s  shopTime=%-3s  checkoutTime=%-3s", hashCode(), Arrays.toString(shoppingCart.toArray()), shopTime, checkoutTime);
     }
 
     /**
