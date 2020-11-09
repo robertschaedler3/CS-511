ad304f4a08b02b8daa88f017cf3a30ebaf854349
Robert Schaedler III <robertschaedler3@gmail.com>
2020-11-09 14:16:53 -0500

    update exercise 3

--- erlang/eb8/ex3.erl
+++ erlang/eb8/ex3.erl
@@ -7,15 +7,15 @@ start(N, I) ->
     [spawn(?MODULE, client, [S, I]) || _ <- lists:seq(1, N)],
     ok.
 
-client(S, I) when I > 0 ->
-    S ! {continue, self()},
-    client(S, I - 1);
-client(S, I) when I == 0 -> S ! {counter, self()}.
+client(S, 0) -> S ! {counter};
+client(S, N) when N > 0 ->
+    S ! {continue},
+    client(S, N - 1).
 
-server(Count) ->
+server(N) ->
     receive
-        {continue, _C} -> server(Count + 1);
-        {counter, _C} ->
-            io:format("counter ~w~n", [Count]),
-            server(Count)
+        {continue} -> server(N + 1);
+        {counter} ->
+            io:format("counter ~w~n", [N]),
+            server(N)
     end.
