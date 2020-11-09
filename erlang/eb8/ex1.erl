b76183390c11558fa39f636e1042bddd9eab1d07
Robert Schaedler III <robertschaedler3@gmail.com>
2020-11-09 11:19:26 -0500

    complete exercise 1

--- erlang/eb8/ex1.erl
+++ erlang/eb8/ex1.erl
@@ -1,19 +1,22 @@
 -module(ex1).
+
 -compile(export_all).
 
 start(N) -> %% Spawns a counter and N turnstile clients
-    C = spawn(?MODULE, counter_server, [0]),
-    [spawn(?MODULE, turnstile, [C, 50]) || _ <- lists:seq(1, N)],
-    C.
+    S = spawn(?MODULE, counter_server, [0]),
+    [spawn(?MODULE, turnstile, [S, 50]) || _ <- lists:seq(1, N)],
+    S.
 
-counter_server(State) -> %% State is the current value of the counter
+counter_server(N) -> %% State is the current value of the counter
     receive
-        {_From, bump, N} -> 
-            counter_server(N + State);
-        {From, read} -> 
-            From!{State},
-            counter_server(State)
+        {bump} -> counter_server(N + 1);
+        {From, read} ->
+            From ! {N},
+            counter_server(N)
     end.
 
-turnstile(C, N) -> %% C is the PID of the counter, and N the number of turns
-    C!{self(), bump, N}.
+turnstile(_S, 0) -> 
+    ok;
+turnstile(S, N) when N > 0 -> 
+    S ! {bump},
+    turnstile(S, N - 1).
